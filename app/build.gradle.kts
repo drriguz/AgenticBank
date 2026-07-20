109ac9c6b3a8a445c0c5ceab16570182e0ae637b
Riguz Lee <dr@riguz.com>
2026-07-20 23:15:44 +0800

    init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.riguz.agenticbank"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.riguz.agenticbank"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
